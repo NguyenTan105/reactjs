2c51c9f1cf0e6f1a13f71d3a9a05332a8b10834d
NguyenTan105 <tannguyenkhacminh112@gmail.com>
2023-10-14 10:02:02 +0700

    update state_5

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "Tan Nguyen",
+    birthday: "10/05/2003",
+  };
+
   render() {
-    return <div>Hello my Component Tan Nguyen</div>;
+    return (
+      <>
+        <div className="frist">My name is {this.state["name"]}</div>
+        <div className="second">My birthday is {this.state.birthday}</div>
+      </>
+    );
   }
 }
 
