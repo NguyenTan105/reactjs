f3c7299496775b8e1dcf8368396c5a6326511a36
NguyenTan105 <tannguyenkhacminh112@gmail.com>
2023-10-15 13:01:20 +0700

    update DOM event_6

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -6,11 +6,32 @@ class MyComponent extends React.Component {
     birthday: "10/05/2003",
   };
 
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handleClickButton = () => {
+    console.log("hit the button");
+    alert("click me");
+  };
+
   render() {
     return (
       <>
-        <div className="frist">My name is {this.state["name"]}</div>
+        <div className="frist">
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={(event) => this.handleOnChangeName(event)}
+          />
+          My name is {this.state["name"]}
+        </div>
         <div className="second">My birthday is {this.state.birthday}</div>
+        <div className="third">
+          <button onClick={() => this.handleClickButton()}>Click me!</button>
+        </div>
       </>
     );
   }
